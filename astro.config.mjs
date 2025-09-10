663ac69c7ee1ee9cbea9fcef7850b4a0988f09ce
Sean Meador <smeador@users.noreply.github.com>
2025-09-10 12:55:28 -0500

    Add Vercel Astro package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   integrations: [mdx(), react(), tailwind()],
-  site: 'https://yourdomain.com', // TODO: Update with actual domain
+
+  // TODO: Update with actual domain
+  site: "https://meador.me",
+
+  adapter: vercel(),
 });
